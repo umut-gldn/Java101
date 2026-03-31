4217b5e79c5a0ac2475dd04b0489082aef56d8e8
umut-gldn <umutgulden33@gmail.com>
2026-04-01 00:54:47 +0300

    yıldızlarla b

---
+++ src/diziler/YildizlarlaB.java
@@ -0,0 +1,26 @@
+package diziler;
+
+public class YildizlarlaB {
+    public static void main(String[] args) {
+
+        String[][] arr = new String[6][4];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (i == 0 || i == 2 || i == 5) {
+                    arr[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    arr[i][j] = " * ";
+                } else {
+                    arr[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : arr) {
+            for (String col : row) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
